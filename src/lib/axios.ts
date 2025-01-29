a9ed0f67ff06f998af10991c845fd532b7e500ff
xcloudzy <amit4.jammu@gmail.com>
2025-01-29 14:30:43 +0530

    fixing bugs

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,9 +3,6 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://blogger-backend-phi.vercel.app/api',
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  }
 });
 
 export default api;
