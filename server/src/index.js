aacf5ae04a412a62a5b2345018b79fa78865bb39
xcloudzy <amit4.jammu@gmail.com>
2025-01-29 14:51:28 +0530

    fixing bugs

--- server/src/index.js
+++ server/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://icloudnote.vercel.app"],
+    origin: ["https://blogger-backend-phi.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
