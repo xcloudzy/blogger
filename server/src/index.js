e00e125a93ece326af7cc09486564db4c150b038
xcloudzy <amit4.jammu@gmail.com>
2025-01-29 13:28:56 +0530

    depoying to vercel

--- server/src/index.js
+++ server/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://blogger-backend-phi.vercel.app",
     credentials: true,
   })
 );
