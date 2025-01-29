f7ea90597a1585c85e243f9a6a27186031f25b54
xcloudzy <amit4.jammu@gmail.com>
2025-01-29 14:25:37 +0530

    depoying to vercel

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -8,13 +8,4 @@ const api = axios.create({
   }
 });
 
-// Add response interceptor for error handling
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error('API Error:', error.response?.data || error.message);
-    return Promise.reject(error);
-  }
-);
-
 export default api;
