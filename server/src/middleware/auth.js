658091b53f13db04dfa9444f182ccf742cae47e2
xcloudzy <amit4.jammu@gmail.com>
2025-01-30 22:19:19 +0530

    as

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,27 +1,8 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
 
 export const protect = async (req, res, next) => {
-  let token;
-
-  token = req.cookies.token;
-
-  if (
-    !token &&
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  }
+  const token = req.cookies.token;
 
   if (!token) {
     return res.status(401).json({ message: "Not authorized" });
